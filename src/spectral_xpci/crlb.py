8b2293abe4ac0a0d7657bf3a502ebd61f26c3997
gjadick <giavanna@uchicago.edu>
2024-11-25 12:24:10 -0600

    update test. TODO: move crlb code to src

--- src/spectral_xpci/crlb.py
+++ src/spectral_xpci/crlb.py
@@ -1,36 +1,65 @@
-import numpy as np
+# import numpy as np
 
-def crlb_2mat_sq(Nx, dx, w, ts, R, mus, deltas):
-    t1, t2 = ts
-    kx = np.fft.fftfreq(Nx, dx)
-    KX, KY = jnp.meshgrid(kx, ky)
-    F = (w**2) * np.sinc(w*KX) * np.sinc(w*KY)
+import jax.numpy as jnp
+from jax.image import scale_and_translate
+from simulate import apply_psf
+from xscatter import PI, get_wavelen, get_delta_beta_mix
 
-    # Define the material-dependent matrices
-    # TODO : reshape Rs so you can vary those instead of mus, deltas
-    K2 = 4 * PI**2 * (KX**2 + KY**2)
-    A11 = mus[0,0] - (R * K2 * deltas[0,0])
-    A12 = mus[1,0] - (R * K2 * deltas[1,0])
-    A21 = mus[0,1] - (R * K2 * deltas[0,1])
-    A22 = mus[1,1] - (R * K2 * deltas[1,1])
-
-    # Signals
-    m1 = np.exp(np.fft.ifft2(t1*F*A11 + t2*F*A12)).real    
-    m2 = np.exp(np.fft.ifft2(t1*F*A21 + t2*F*A22)).real
-
-    # Partial derivatives
-    dm11 = m1 * np.fft.ifft2(F*A11).real
-    dm12 = m1 * np.fft.ifft2(F*A12).real
-    dm21 = m2 * np.fft.ifft2(F*A21).real
-    dm22 = m2 * np.fft.ifft2(F*A22).real    
-
-    # Fisher information
-    I11 = np.sum((dm11**2/m1)   + (dm21**2/m2))
-    I12 = np.sum((dm11*dm12/m1) + (dm21*dm22/m2))
-    I21 = np.sum((dm12*dm11/m1) + (dm22*dm21/m2))
-    I22 = np.sum((dm12**2/m1)   + (dm22**2/m2))
-    I = np.array([[I11, I12], [I21, I22]])
-
-    CRLBs = np.linalg.inv(I).diagonal()
+def detect_img(x, in_px, out_Nx, out_px, det_fwhm=None, normalize=False):
+    """
+    create the detector resampling function
+    this should be applied to both signals m and partials dm/dt before the Fisher info calc.
+    """
+    out_shape = [out_Nx, out_Nx]
+    scale = jnp.array([in_px/out_px, in_px/out_px])
+    translation = -0.5 * (scale*jnp.array(x.shape) - jnp.array(out_shape))
+    total = x.sum()
+    x = scale_and_translate(x, out_shape, (0,1), scale, translation, method='linear')
+    if normalize:
+        x = x * (total / x.sum())
+    if det_fwhm is not None:
+        x = apply_psf(x, x.shape[0]*out_px, out_px, fwhm=det_fwhm, psf='lorentzian', kernel_width=0.1)
+    return x
     
-    return CRLBs
+
+def crlb_2matdecomp_sq(matinfo1, matinfo2, energies, propdists, w, Nx, dx, det_Nx, det_dx, det_fwhm):
+    
+    kx = np.fft.fftfreq(Nx, d=dx)
+    KX, KY = np.meshgrid(kx, kx)
+    sinc2D = w**2 * np.sinc(w*KX) * np.sinc(w*KY) / dx**2    # normalize rect amplitude = 1
+
+    def get_Aij(E_i, R_i, mat_j, p_j):
+        delta_ij, beta_ij = get_delta_beta_mix(mat_j, E_i, p_j)
+        mu_ij = 2 * (2 * PI / get_wavelen(E_i)) * beta_ij
+        return mu_ij - (R_i * (KX**2 + KY**2) * delta_ij) 
+    
+    def get_mi(E_i, R_i, matinfo1, matinfo2, I0):
+        mat1, p1, t1 = matinfo1
+        mat2, p2, t2 = matinfo2
+        Ai1 = get_Aij(E_i, R_i, mat1, p1)
+        Ai2 = get_Aij(E_i, R_i, mat2, p2)  
+        iFT = jnp.fft.fftshift(jnp.fft.ifft2(sinc2D * (Ai1*t1 + Ai2*t2))).real
+        return I0 * jnp.exp(-detect_img(iFT, dx, det_Nx, det_dx, det_fwhm))
+        
+    def get_dmi_dAij(mi, Aij):
+        iFT = np.fft.fftshift(np.fft.ifft2(sinc2D * Aij)).real
+        return mi * detect_img(iFT, dx, det_Nx, det_dx, det_fwhm)
+    
+    F = np.zeros([2,2])
+    for i in range(len(energies)):
+        E_i = energies[i]
+        R_i = R
+        mi = get_mi(E_i, R, matinfo1, matinfo2, I0)
+        dmi_dA1 = get_dmi_dAij(mi, get_Aij(E_i, R_i, matinfo1[0], matinfo1[1]))
+        dmi_dA2 = get_dmi_dAij(mi, get_Aij(E_i, R_i, matinfo2[0], matinfo2[1]))
+        F[0,0] += jnp.sum(dmi_dA1**2 / mi)
+        F[0,1] += jnp.sum(dmi_dA1 * dmi_dA2 / mi)
+        F[1,0] += jnp.sum(dmi_dA2 * dmi_dA1 / mi)
+        F[1,1] += jnp.sum(dmi_dA2**2 / mi)
+    crlbs = np.linalg.diagonal(np.linalg.inv(F))
+
+    return crlbs
+
+
+
+
